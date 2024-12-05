e9a22b3a0d28f5d4748eb20031608e9a73afb30a
abobabo91 <elekesabo@gmail.com>
2024-12-05 12:40:03 -0500

    asd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import numpy as npimport
+from st_files_connection import FilesConnection
 
 st.title('Startup finder')
 
 st.write('Hi')
 
 
+conn = st.connection('gcs', type=FilesConnection)
+df = conn.read("success_new/embeddings.csv", input_format="csv", ttl=600)
 
 
-
-
-
+df
 
 
 
