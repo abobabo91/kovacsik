4d0a6471920460ce070799d1bc621e9f43a1dd19
abobabo91 <elekesabo@gmail.com>
2024-12-05 14:07:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,12 @@ import pandas as pd
 import numpy as np
 from st_files_connection import FilesConnection
 
-import openai
 from openai import OpenAI
 
 from gensim.models.keyedvectors import KeyedVectors
 
 openai.organization = "org-i7aicv7Qc0PO4hkTCT4N2BqR"
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = st.secrets['openai']["OPENAI_API_KEY"]
 
 
 st.title('Startup finder')
@@ -32,3 +31,4 @@ client = OpenAI(api_key=openai.api_key)
 model = "text-embedding-3-small"  
 response = client.embeddings.create(input=[investment_thesis], model=model).data[0].embedding
 
+response
