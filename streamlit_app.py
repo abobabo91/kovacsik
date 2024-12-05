ad3a0e208b92804796c40a9b3aed4e300e9866e2
abobabo91 <elekesabo@gmail.com>
2024-12-05 13:59:01 -0500

    openai api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,34 @@
 import streamlit as st
 import pandas as pd
-import numpy as npimport
+import numpy as np
 from st_files_connection import FilesConnection
+
+import openai
+from openai import OpenAI
+
+from gensim.models.keyedvectors import KeyedVectors
+
+openai.organization = "org-i7aicv7Qc0PO4hkTCT4N2BqR"
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
 
 st.title('Startup finder')
 
 st.write('Hi')
 
-
 conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("success_new/embeddings.csv", input_format="csv", ttl=600)
+
+embeddings = conn.read("success_new/embeddings.csv", input_format="csv", ttl=600)
 
 
-df
+embeddings.head()
+
+
+
+investment_thesis = "AI agent"
+st.write(investment_thesis)
+
+client = OpenAI(api_key=openai.api_key)
+model = "text-embedding-3-small"  
+response = client.embeddings.create(input=[investment_thesis], model=model).data[0].embedding
 
